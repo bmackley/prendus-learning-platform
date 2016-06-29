c4d1d8d96780e277a1fe4ad9654e38b2877f26a7
Ben Mackley <benjaminmackley@gmail.com>
2016-06-29 16:28:30 -0600

    got everything working. This is the base commit. Will branch off develop and ben from here

--- app/client/components/sign-up/sign-up.ts
+++ app/client/components/sign-up/sign-up.ts
@@ -4,30 +4,14 @@ Polymer({
     'signup-submit.tap': 'specialTap'
   },
   specialTap: function(e){
-    var ref = new Firebase("https://prendus.firebaseio.com/");
-    ref.createUser({
-      email: this.$.formEmail.value,
-      password: this.$.formPassword.value
-    }, function(error, userData) {
-      if (error) {
-        switch (error.code) {
-          case "EMAIL_TAKEN":
-            alert("The new user account cannot be created because the email is already in use.");
-            break;
-          case "INVALID_EMAIL":
-            alert("The specified email is not a valid email.");
-            break;
-          default:
-            alert("Error creating user:", error);
-        }
-      } else {
-        console.log("Successfully created user account with uid:", userData.uid);
-      }
-    });
+    Actions.createUser.execute(this, this.$.formEmail.value, this.$.formPassword.value);
   },
   properties: {
 
-    },
+  },
+  mapStateToThis: function(e) {
+
+  },
   ready: function(e){
     function _submit(){
       console.log('hello world')
