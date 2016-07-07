b3c8c9ed9eb3db5c61333683b9d93cf724640ea4
Ben Mackley <benjaminmackley@gmail.com>
2016-07-07 15:54:55 -0600

    got the login, the signup, and the edit profile working

--- app/client/components/profile-element/profile-element.ts
+++ app/client/components/profile-element/profile-element.ts
@@ -5,64 +5,63 @@ Polymer({
   listeners: {
   },
   mapStateToThis: function(e) {
+    //this.currentUser = e.detail.state.currentUser
     this.firstName = e.detail.state.currentUser.firstName;
     this.lastName = e.detail.state.currentUser.lastName;
     this.institution = e.detail.state.currentUser.institution;
     this.pastEmail = e.detail.state.currentUser.email;
     this.email = e.detail.state.currentUser.email;
-    console.log('sign in mapstate s', e.detail)
-    if(e.detail.state.error.message){
-      this.updateProfileToastText = e.detail.state.error.message
-      this.$.updateProfileToast.open();
-    }
   },
-  changeProfile: function(e) {
-    console.log(e)
-    console.log(this.$.firstName.value)
-    console.log(this.$.lastName.value)
-    console.log('form email', this.$.updateEmail.value)
-    console.log('saved email', this.email)
-
-    let submitValue = {
-      firstName: this.$.firstName.value,
-      lastName: this.$.lastName.value,
-      institution: this.$.institution.value,
-      email:  this.$.updateEmail.value,
-    }
+  changeProfile: async function(e) {
     if(this.$.updateEmail.value != this.pastEmail){
-      //Do a confirmation box
-      console.log('email is different')
       this.$.confirmEmailChange.open();
-      console.log('canceled', this.$.confirmEmailChange.canceled);
     }else{
-      submitValue.email = '';
-      Actions.updateUser.execute(this, submitValue);
-    }
-
-  },
-  closeOverlay: function(e){
-    console.log('closeOverlay e', e);
-    console.log('Confirmed', e.detail.confirmed);
-    if(e.detail.confirmed === true){
-      console.log('firstName', this.$.firstName.value)
       let submitValue = {
         firstName: this.$.firstName.value,
         lastName: this.$.lastName.value,
         institution: this.$.institution.value,
         email:  this.$.updateEmail.value,
       }
-      Actions.updateUser.execute(this, submitValue)
+      try{
+        await Actions.updateUser.execute(this, submitValue);
+        this.updateProfileSuccessToastText = 'Profile Successfully Updated';
+        this.$.updateProfileSuccessToast.open();
+      }
+      catch(error){
+        this.updateProfileErrorToastText = error.message;
+        this.$.updateProfileErrorToast.open();
+      }
     }
-    console.log('cancelled', e.detail.canceled);
+
+  },
+  closeOverlay: async function(e){
+    if(e.detail.confirmed === true){
+      let submitValue = {
+        firstName: this.$.firstName.value,
+        lastName: this.$.lastName.value,
+        institution: this.$.institution.value,
+        email:  this.$.updateEmail.value,
+        runEmail: true,
+      }
+      try{
+        await Actions.updateUser.execute(this, submitValue);
+        this.updateProfileSuccessToastText = 'Profile Updated Successfully';
+        this.$.updateProfileSuccessToast.open();
+      }
+      catch(error){
+        this.updateProfileErrorToastText = error.message;
+        this.$.updateProfileErrorToast.open();
+      }
+    }
   },
   properties: {
   },
 
   ready: function(e){
-    this.$.updateProfileToast.fitInto = this.$.toastTarget;
+    this.$.updateProfileErrorToast.fitInto = this.$.toastTarget;
+    this.$.updateProfileSuccessToast.fitInto = this.$.toastTarget;
     // this.$.form.addEventListener('iron-form-presubmit', function(event) {
     //   event.preventDefault();
-    //   console.log(this.$.form.getAttribute('action'))
     // });
   }
 });
--- app/client/redux/reducers.ts
+++ app/client/redux/reducers.ts
@@ -2,87 +2,84 @@ import {InitialState} from './initial-state.ts';
 import {Actions} from './actions.ts';
 
 export function rootReducer(state = InitialState, action) {
-    state.error.message = '';
     if(action.successMessage){
       state.success.message = action.successMessage
     }else{
       state.success.message = '';
     }
     switch(action.type) {
-        case Actions.setCurrentUser.type: {
-            const newState = Object.assign({}, state);
-            newState.currentUser.email = action.email;
-            return newState;
-        }
-        case Actions.checkUserAuth.type: {
-          console.log('Reducer action user auth', action)
+      case Actions.createUser.type: {
           const newState = Object.assign({}, state);
-          newState.currentUser.email = action.email;
-          newState.currentUser.firstName = action.firstName;
-          newState.currentUser.lastName = action.lastName;
-          newState.currentUser.institution = action.institution;
+          newState.currentUser = action.currentUser;
           return newState;
-        }
-        case Actions.addConcept.type: {
-          console.log('reducers add concept', action)
+      }
+      case Actions.loginUser.type: {
           const newState = Object.assign({}, state);
-          console.log('new state', state)
-          newState.concepts[action.key] = action;
-          console.log("reducers add concept new state1 ", newState)
-          newState.newConcept = newState.concepts[action.key];
-          console.log("reducers add concept new state 2", newState)
+          newState.currentUser = action.currentUser;
           return newState;
-        }
-        case Actions.setConcepts.type: {
-            const newState = Object.assign({}, state);
-            newState.concepts = {
-                // uid: action.uid,
-                title: action.title,
-                // pos: action.pos,
-                // creator: action.creator
-            };
-            return newState;
-        }
-        case Actions.getConcepts.type: {
-            const newState = Object.assign({}, state);
-            newState.concepts = action.concepts;
-            return newState;
-        }
-        case Actions.deleteConcept.type: {
+      }
+      case Actions.checkUserAuth.type: {
+        console.log('Reducer action user auth', action)
+        const newState = Object.assign({}, state);
+        newState.currentUser.email = action.email;
+        newState.currentUser.firstName = action.firstName;
+        newState.currentUser.lastName = action.lastName;
+        newState.currentUser.institution = action.institution;
+        return newState;
+      }
+      case Actions.addConcept.type: {
+        console.log('reducers add concept', action)
+        const newState = Object.assign({}, state);
+        console.log('new state', state)
+        newState.concepts[action.key] = action;
+        console.log("reducers add concept new state1 ", newState)
+        newState.newConcept = newState.concepts[action.key];
+        console.log("reducers add concept new state 2", newState)
+        return newState;
+      }
+      case Actions.setConcepts.type: {
           const newState = Object.assign({}, state);
-          console.log('key', action.key)
-          console.log('concepts', state )
-          delete newState.concepts[action.conceptKey];
-          newState.deletedConcept = action.conceptKey;
-          console.log('Delete old State', state)
-          console.log('Delete new State', newState)
+          newState.concepts = {
+              // uid: action.uid,
+              title: action.title,
+              // pos: action.pos,
+              // creator: action.creator
+          };
           return newState;
-        }
-        case Actions.logOutUser.type: {
+      }
+      case Actions.getConcepts.type: {
           const newState = Object.assign({}, state);
-          console.log('user has been logged out actions')
-          newState.currentUser = {uid: '', username: '', permissions: ''};
+          newState.concepts = action.concepts;
           return newState;
-        }
-        case Actions.updateUser.type: {
-          const newState = Object.assign({}, state);
-          console.log('Action', action)
-          newState.currentUser = {firstName: action.user.firstName, lastName: action.user.lastName, institution: action.user.institution, email: action.user.email};
-          return newState;
-        }
-        case Actions.setURL.type: {
-          const newState = Object.assign({}, state);
-          newState.URL = action.URL;
-          return newState;
-        }
-        case Actions.displayError.type: {
-          const newState = Object.assign({}, state);
-          console.log(action.error)
-          newState.error.message = action.error.message;
-          return newState;
-        }
-        default: {
-            return state;
-        }
+      }
+      case Actions.deleteConcept.type: {
+        const newState = Object.assign({}, state);
+        console.log('key', action.key)
+        console.log('concepts', state )
+        delete newState.concepts[action.conceptKey];
+        newState.deletedConcept = action.conceptKey;
+        console.log('Delete old State', state)
+        console.log('Delete new State', newState)
+        return newState;
+      }
+      case Actions.logOutUser.type: {
+        const newState = Object.assign({}, state);
+        console.log('user has been logged out actions')
+        newState.currentUser = {uid: '', username: '', permissions: ''};
+        return newState;
+      }
+      case Actions.updateUser.type: {
+        const newState = Object.assign({}, state);
+        newState.currentUser = action.user;
+        return newState;
+      }
+      case Actions.setURL.type: {
+        const newState = Object.assign({}, state);
+        newState.URL = action.URL;
+        return newState;
+      }
+      default: {
+          return state;
+      }
     }
 }
