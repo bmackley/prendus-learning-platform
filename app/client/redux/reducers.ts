d12bb985b22aa94f9798b7d7fd18ae972a4dc3c8
bmackley <bmackley@byu.edu>
2016-07-12 16:20:18 -0600

    Update reducers.ts

--- app/client/redux/reducers.ts
+++ app/client/redux/reducers.ts
@@ -8,51 +8,6 @@ export function rootReducer(state = InitialState, action) {
       state.success.message = '';
     }
     switch(action.type) {
-        case Actions.setCurrentUser.type: {
-            const newState = Object.assign({}, state);
-            newState.currentUser.email = action.email;
-            return newState;
-        }
-        case Actions.checkUserAuth.type: {
-          console.log('Reducer action user auth', action)
-          const newState = Object.assign({}, state);
-          newState.currentUser.email = action.email;
-          newState.currentUser.firstName = action.firstName;
-          newState.currentUser.lastName = action.lastName;
-          newState.currentUser.institution = action.institution;
-          return newState;
-        }
-        case Actions.addConcept.type: {
-          console.log('reducers add concept', action)
-          const newState = Object.assign({}, state);
-          console.log('new state', state)
-          newState.concepts[action.key] = action;
-          console.log("reducers add concept new state1 ", newState)
-          newState.newConcept = newState.concepts[action.key];
-          console.log("reducers add concept new state 2", newState)
-          return newState;
-        }
-        case Actions.setConcepts.type: {
-            const newState = Object.assign({}, state);
-            newState.concepts = {
-                // uid: action.uid,
-                title: action.title,
-                // pos: action.pos,
-                // creator: action.creator
-            };
-            return newState;
-        }
-        case Actions.getConcepts.type: {
-            const newState = Object.assign({}, state);
-            newState.concepts = action.concepts;
-            return newState;
-        }
-        case Actions.deleteConcept.type: {
-          const newState = Object.assign({}, state);
-          delete newState.concepts[action.conceptKey];
-          newState.deletedConcept = action.conceptKey;
-          return newState;
-        }
       case Actions.createUser.type: {
           const newState = Object.assign({}, state);
           newState.currentUser = action.currentUser;
