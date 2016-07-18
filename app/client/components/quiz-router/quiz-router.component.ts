9af79e595649391f9ceca752857586fb9d00210e
Jordan Last <jordan.michael.last@gmail.com>
2016-07-18 17:19:22 -0600

    questions reload when saved

--- app/client/components/quiz-router/quiz-router.component.ts
+++ app/client/components/quiz-router/quiz-router.component.ts
@@ -1,10 +1,14 @@
 import {FirebaseService} from '../../node_modules/prendus-services/services/firebase.service.ts';
+import {Actions} from '../../redux/actions.ts';
 
 class QuizRouterComponent {
     public is: string;
     public selected: number;
     public jwt: string;
     public currentEditQuestionId: string;
+    public conceptId: string;
+    public quizId: string;
+    public properties: any;
 
     beforeRegister() {
         this.is = 'prendus-quiz-router';
@@ -32,6 +36,21 @@ class QuizRouterComponent {
         //TODO this is evil, make sure to remove it once edit problem component can reload itself in response to property changes
 
         this.selected = 1;
+    }
+
+    routeToCreateQuestion() {
+        //TODO this is evil, make sure to remove it once edit problem component can reload itself in response to property changes
+        const editProblemComponent = this.querySelector('#editProblemComponent');
+        editProblemComponent.originalText = '';
+        editProblemComponent.originalCode = '';
+        //TODO this is evil, make sure to remove it once edit problem component can reload itself in response to property changes
+
+        this.selected = 1;
+    }
+
+    async questionSaved() {
+        const quizEditorComponent = this.querySelector('#quizEditorComponent');
+        quizEditorComponent.manuallyReloadQuestions();
     }
 
     viewQuizEditor() {
