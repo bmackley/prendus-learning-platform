93f1ff04bab770dcb87cb9a678e802a1c91c5602
Ben Mackley <benjaminmackley@gmail.com>
2016-08-04 20:22:53 -0600

    fixed the unseen merge issue

--- app/client/redux/actions.ts
+++ app/client/redux/actions.ts
@@ -231,6 +231,8 @@ const checkUserAuth = {
       if(loggedInUser){
         let userData = await UserModel.getMetaDataById(loggedInUser.uid, 'metaData');
         userData.uid = loggedInUser.uid; //OK because its being created here.
+        console.log('in the check user auth')
+        console.log('user data', userData)
         context.action = {
           type: Actions.checkUserAuth.type,
           currentUser: userData,
