8c2960283b005aa7b41906d33d0336850c6be0ff
Ben Mackley <benjaminmackley@gmail.com>
2016-08-05 17:03:37 -0600

    got the routing working with the edit and view course pages.

--- app/client/components/app-element/app-element.ts
+++ app/client/components/app-element/app-element.ts
@@ -1,22 +1,5 @@
 
 import {rootReducer} from '../../redux/reducers.ts';
-// export class AppComponent {
-//   public is: string;
-//   public observers: string[];
-//   public rootReducer: any;
-//
-//   beforeRegister() {
-//     this.is = 'app-element';
-//     this.observers = [
-//         'init(rootReducer)'
-//     ];
-//   }
-//   async init() {
-//     console.log(rootReducer)
-//     this.rootReducer = rootReducer;
-//   }
-// }
-// Polymer(AppComponent)
 
 Polymer({
   is: "app-element",
