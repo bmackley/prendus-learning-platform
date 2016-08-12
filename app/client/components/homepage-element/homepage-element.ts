f2bf75005db658442e119f6b13f3b0b204ee7666
Ben Mackley <benjaminmackley@gmail.com>
2016-08-11 21:13:01 -0600

    putting some finishing touches on the typescript

--- app/client/components/homepage-element/homepage-element.ts
+++ app/client/components/homepage-element/homepage-element.ts
@@ -1,7 +1,15 @@
-Polymer({
-  is: "index-element",
-  properties: {
-    },
-  ready: function(e){
+import {Actions} from '../../redux/actions.ts';
+
+export class HomepageComponent {
+  public is: string;
+  public username: string;
+
+  beforeRegister() {
+    this.is = 'index-element';
   }
-});
+  mapStateToThis(e) {
+    const state = e.detail.state
+    this.username = state.currentUser.email;
+  }
+}
+Polymer(HomepageComponent);
--- app/client/components/navbar/navbar-element.ts
+++ app/client/components/navbar/navbar-element.ts
@@ -12,6 +12,7 @@ export class CourseNavbarComponent {
   }
   mapStateToThis(e: StatechangeEvent) {
     const state = e.detail.state
+    console.log('navbar state', state.currentUser)
     this.username = state.currentUser.metaData.email;
   }
   changeURL(e: any){
