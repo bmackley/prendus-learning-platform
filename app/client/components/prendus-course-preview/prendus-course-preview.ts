50d4fb3488e777f1ac5e6681e9989050ed70a7d9
Jordan Last <jordan.michael.last@gmail.com>
2016-08-22 12:23:34 -0600

    starring fully functional

--- app/client/components/prendus-course-preview/prendus-course-preview.ts
+++ app/client/components/prendus-course-preview/prendus-course-preview.ts
@@ -1,30 +1,90 @@
+import {Course} from '../../node_modules/prendus-services/interfaces/course.interface.ts';
+import {StatechangeEvent} from '../../interfaces/statechange-event.interface.ts';
+import {User} from '../../node_modules/prendus-services/interfaces/user.interface.ts';
+import {Actions} from '../../redux/actions.ts';
+
 class PrendusCoursePreview {
     public is: string;
     public properties: any;
+    public course: Course;
+    public starIcon: string;
+    public user: User;
+    public numStars: number;
 
     beforeRegister() {
         this.is = 'prendus-course-preview';
         this.properties = {
             course: {
-                type: Object
+                type: Object,
+                observer: 'init'
             }
         };
     }
 
+    init(course: Course) {
+        this.starIcon = `icons:star-border`
+    }
+
+    async starClick(e: any) {
+        if (this.user) {
+            if (this.user.starredCourses) {
+                if (this.user.starredCourses[this.course.id]) {
+                    await Actions.unstarCourse(this, this.course.id);
+                }
+                else {
+                    await Actions.starCourse(this, this.course.id);
+                }
+            }
+            else {
+                await Actions.starCourse(this, this.course.id);
+            }
+
+            Actions.checkUserAuth.execute(this);
+            Actions.getStarredCoursesByUser(this, this.user.metaData.uid);
+            Actions.getCoursesByVisibility(this, 'public');
+            Actions.getCoursesByUser.execute(this);
+            Actions.getStarredCoursesByUser(this, this.user.metaData.uid);
+            Actions.getSharedCoursesByUser(this, this.user.metaData.uid);
+        }
+        else {
+            alert('You must be logged in to star a course');
+        }
+    }
+
     editCourse(e: any) {
-      const location = `/courses/edit/${e.target.id}`
-      window.history.pushState({}, '', location);
-      this.fire('location-changed', {}, {node: window});
+        const location = `/courses/edit/${this.course.id}`
+        window.history.pushState({}, '', location);
+        this.fire('location-changed', {}, {node: window});
     }
 
     viewCourse(e: any) {
-      try{
-        const location = `/courses/view/${e.target.id}`
+        const location = `/courses/view/${this.course.id}`
         window.history.pushState({}, '', location);
         this.fire('location-changed', {}, {node: window});
-      }catch(error){
-        alert(error);
-      }
+    }
+
+    mapStateToThis(e: StatechangeEvent) {
+        const state = e.detail.state;
+
+        this.user = state.currentUser;
+        this.numStars = Object.keys(this.course.userStars || {}).length;
+
+        if (this.user && this.course) {
+            if (this.user.starredCourses) {
+                if (this.user.starredCourses[this.course.id]) {
+                    this.starIcon = 'icons:star';
+                }
+                else {
+                    this.starIcon = 'icons:star-border';
+                }
+            }
+            else {
+                this.starIcon = `icons:star-border`;
+            }
+        }
+        else {
+            this.starIcon = `icons:star-border`;
+        }
     }
 }
 
