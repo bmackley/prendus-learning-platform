129c8680181b0551c1f366afd469abe4bac399a9
Brandon Blatter <intcreator@users.noreply.github.com>
2016-09-07 16:44:04 -0600

    Implement routing for landing page

--- app/client/components/prendus-router/prendus-router.ts
+++ app/client/components/prendus-router/prendus-router.ts
@@ -2,6 +2,8 @@ import {StatechangeEvent} from '../../interfaces/statechange-event.interface.ts'
 
 class PrendusRouter {
   public is: string;
+  public username: string;
+  public loggedIn: string;
   public mainViewToShow: 'routes' | 'spinner';
 
   beforeRegister() {
@@ -10,7 +12,8 @@ class PrendusRouter {
 
   mapStateToThis(e: StatechangeEvent) {
       const state = e.detail.state;
-
+      this.username = state.currentUser.metaData.email;
+      this.loggedIn = this.username ? "true" : "false";
       this.mainViewToShow = state.mainViewToShow;
   }
 }
