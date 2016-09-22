d4dcee6b1342dbedb6a7c6ae6a2faa9087fa6847
Brandon Blatter <intcreator@users.noreply.github.com>
2016-09-22 16:43:56 -0600

    Redo layout for quiz editor

--- app/client/components/prendus-edit-question-router/prendus-edit-question-router.ts
+++ app/client/components/prendus-edit-question-router/prendus-edit-question-router.ts
@@ -3,9 +3,20 @@ import {Actions} from '../../redux/actions.ts';
 class PrendusEditQuestionRouter {
     public is: string;
     public jwt: string;
+    public observers: Array;
 
     beforeRegister() {
         this.is = 'prendus-edit-question-router';
+        this.observers = [
+          '_routeChanged(route.*)'
+        ]
+    }
+
+    _routeChanged(route) {
+      if(!route.data) {
+        this.originalText = '';
+        this.originalCode = '';
+      }
     }
 
     editProblemComponentLoaded(e) {
