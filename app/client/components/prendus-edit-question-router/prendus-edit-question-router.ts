f8b8ba0916bf032f29ab3cb28cdd42248186d414
Ben Mackley <benjaminmackley@gmail.com>
2016-09-22 17:16:49 -0600

    merge with brandons latest changes. Working on updating the quizzes

--- app/client/components/prendus-edit-question-router/prendus-edit-question-router.ts
+++ app/client/components/prendus-edit-question-router/prendus-edit-question-router.ts
@@ -3,19 +3,35 @@ import {Actions} from '../../redux/actions.ts';
 class PrendusEditQuestionRouter {
     public is: string;
     public jwt: string;
+    public conceptId: string;
+    public quizId: string;
+    public properties: Object;
     public errorMessage: string;
     public successMessage: string;
 
     beforeRegister() {
         this.is = 'prendus-edit-question-router';
+        this.properties = {
+            route: {
+              type: Object,
+              observer: 'setData'
+            }
+        };
     }
-
+    setData() {
+      console.log('setting route', this.route)
+      console.log('setting data', this.data)
+      this.conceptId = this.data.conceptId;
+      this.quizId = this.data.quizId;
+    }
     editProblemComponentLoaded(e) {
         Actions.hideMainSpinner(this);
     }
 
     backToQuiz(){
-      window.history.back();
+      //Would like to go to this with the URL, but getting the info
+      //window.history.back();
+      window.location.href = `courses/edit-quiz/concept/${this.conceptId}/quiz/${this.quizId}`
     }
 
     mapStateToThis(e) {
@@ -25,9 +41,11 @@ class PrendusEditQuestionRouter {
 
     async questionSaved() {
         //TODO this is evil, figure out another way to manually reload the questions without a DOM search
+        this.successMessage = ""
+        this.successMessage = "Question saved successfully"
         const quizEditorComponent = document.getElementById('quizEditorComponent');
         quizEditorComponent.manuallyReloadQuestions();
-        this.successMessage = 'Question saved successfully';
+        this.backToQuiz
         //TODO this is evil, figure out another way to manually reload the questions without a DOM search
 
     }
