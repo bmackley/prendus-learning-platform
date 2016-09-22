65fa50baa403b5b3b2114a64562d90a65caeef8a
Brandon Blatter <intcreator@users.noreply.github.com>
2016-09-22 16:50:58 -0600

    Remove ignored files

--- app/client/components/prendus-edit-question-router/prendus-edit-question-router.ts
+++ app/client/components/prendus-edit-question-router/prendus-edit-question-router.ts
@@ -3,20 +3,9 @@ import {Actions} from '../../redux/actions.ts';
 class PrendusEditQuestionRouter {
     public is: string;
     public jwt: string;
-    public observers: Array;
 
     beforeRegister() {
         this.is = 'prendus-edit-question-router';
-        this.observers = [
-          '_routeChanged(route.*)'
-        ]
-    }
-
-    _routeChanged(route) {
-      if(!route.data) {
-        this.originalText = '';
-        this.originalCode = '';
-      }
     }
 
     editProblemComponentLoaded(e) {
