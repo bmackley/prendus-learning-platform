75a38b5c6317742a36194cfdd28f1a9141293d1f
Matt Crowder <mcrowder65@gmail.com>
2016-11-04 08:24:49 -0600

    fixed tag display problem when adding new course, still need to get display course working properly.

--- app/client/components/prendus-homepage/prendus-homepage.ts
+++ app/client/components/prendus-homepage/prendus-homepage.ts
@@ -5,7 +5,7 @@ import {StatechangeEvent} from '../../interfaces/statechange-event.interface.ts'
 class PrendusHomepage {
     public is: string;
     public publicCourses: Course[];
-
+    public errorMessage: string;
     beforeRegister() {
         this.is = 'prendus-homepage';
     }
@@ -20,7 +20,29 @@ class PrendusHomepage {
         window.history.pushState({}, '', `/courses/view/${courseId}`);
         this.fire('location-changed', {}, {node: window});
     }
+    async displayCourse(course: Course) {
+      try {//TODO figure out how to embed async in html
+        return false;
+        if(!course.concepts) {
+          return false;
+        }
+        const concepts: any = course.concepts;
+        const conceptIds: string[] = Actions.courseConceptsObjectToStringArray(course);
+        // If any concept inside of the course contains a video or a quiz,
+        // then we want to display the course to the user
+        for(let i = 0; i < conceptIds.length; i++) {
+          const hasQuizOrVideo: boolean = await Actions.conceptContainsQuizOrVideo(conceptIds[i]);
+          if(hasQuizOrVideo) {
+            return true;
+          }
+        }
+        return false;
+      } catch(error) {
+        this.errorMessage = '';
+        this.errorMessage = error;
+      }
 
+    }
     async starCourse(e: any) {
         const courseId = e.model.item.courseId;
 
