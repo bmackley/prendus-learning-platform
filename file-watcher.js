7bffa6713ad7a6b6ac54ad56dcfb9df0c056164f
Matt Crowder <mcrowder65@gmail.com>
2016-12-08 11:28:44 -0700

    change 2

--- file-watcher.js
+++ file-watcher.js
@@ -17,8 +17,7 @@ chokidar.watch([
     "app/client/components/*/*",
     "app/client/redux/*",
     "app/client/styles/*",
-    "app/client/interfaces/*",
-    "app/client/node_modules/prendus-services/*/*"
+    "app/client/interfaces/*"
 ]).on('change', (path) => {
 
     const fileEnding = path.substr(path.lastIndexOf('.') + 1);
