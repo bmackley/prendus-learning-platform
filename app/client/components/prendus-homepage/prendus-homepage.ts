56dec4e079413f17b0a2e91c75d57eac0b0e7cb5
Matt Crowder <mcrowder65@gmail.com>
2016-12-15 13:26:27 -0700

    you can infinitely scroll courses now, but it's really slow and buggy. Maybe we should consider querying the course instead of just id's

--- app/client/components/prendus-homepage/prendus-homepage.ts
+++ app/client/components/prendus-homepage/prendus-homepage.ts
@@ -27,25 +27,23 @@ class PrendusHomepage {
     }
 
     async next(e: any): Promise<void> {
-      let threshold: any = this.querySelector('#scrollThreshold');
-      console.log(threshold.lowerTriggered);
-      const word: string = 'hello';
-      for(let i: number = 0; i < 100; i++) {
-        this.words = this.words ? [...this.words, `${word} ${this.words.length + 1}`] : [`${word} 1`];
-      }
-      // console.log(this.words);
-      // console.log(this.words.length);
-      // const amt: number = 9;
-      // const ids: string[] = await CourseModel.getNext(amt);
-      // const courseIds: string[] = this.publicCourses.map((course) => course.id);
-      // const renderIds: string[] = ids.length === amt ? ids : ids.filter((id) => {
-      //   if(courseIds.indexOf(id) === -1) {
-      //     return id;
-      //   }
-      // });
-      // const courses: Course[] = await CourseModel.resolveCourseIds(renderIds);
-      // const newCourses: Course[] = this.publicCourses.concat(courses);
-      // await Actions.reloadPublicCourses(this, newCourses);
+      // let threshold: any = this.querySelector('#scrollThreshold');
+      // console.log(threshold.lowerTriggered);
+      // const word: string = 'hello';
+      // for(let i: number = 0; i < 100; i++) {
+      //   this.words = this.words ? [...this.words, `${word} ${this.words.length + 1}`] : [`${word} 1`];
+      // }
+      const amt: number = 3;
+      const ids: string[] = await CourseModel.getNext(amt);
+      const courseIds: string[] = this.publicCourses.map((course) => course.id);
+      const renderIds: string[] = ids.length === amt ? ids : ids.filter((id) => {
+        if(courseIds.indexOf(id) === -1) {
+          return id;
+        }
+      });
+      const courses: Course[] = await CourseModel.resolveCourseIds(renderIds);
+      const newCourses: Course[] = this.publicCourses.concat(courses);
+      await Actions.reloadPublicCourses(this, newCourses);
 
     }
     async starCourse(e: any): Promise<void> {
