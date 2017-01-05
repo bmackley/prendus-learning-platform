4a67054584d53fd868a2a94d0b9bf6d80a4daae1
Matt Crowder <mcrowder65@gmail.com>
2017-01-05 14:43:23 -0700

    got rid of prendus_env error

--- app/client/components/prendus-homepage/prendus-homepage.ts
+++ app/client/components/prendus-homepage/prendus-homepage.ts
@@ -36,7 +36,7 @@ class PrendusHomepage {
         // and have the public courses display. So I display the
         // courses by visibility, and when the user scrolls down
         // to the bottom, the next courses will load right away.
-        this.baseRef = new Firebase(`https://prendus-${window.PRENDUS_ENV}.firebaseio.com/courses`);
+        this.baseRef = new Firebase(`https://prendus-${window['PRENDUS_ENV']}.firebaseio.com/courses`);
         this.scrollRef = new Firebase.util.Scroll(this.baseRef, 'public');
         await this.loadMoreData(null);
       } catch(error) {
