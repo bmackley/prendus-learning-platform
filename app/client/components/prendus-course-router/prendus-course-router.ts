469a36c7346ba8788808b853b24db9baab95661e
Matt Crowder <mcrowder65@gmail.com>
2017-01-12 12:39:53 -0700

    always import

--- app/client/components/prendus-course-router/prendus-course-router.ts
+++ app/client/components/prendus-course-router/prendus-course-router.ts
@@ -1,5 +1,4 @@
 import {Actions} from '../../redux/actions';
-
 class PrendusCourseRouter {
   public is: string;
   public selected: number;
@@ -23,27 +22,27 @@ class PrendusCourseRouter {
     const baseRoute: string = route.split('/')[1];
     switch(baseRoute) {
       case 'home': {
-        this.importHref('components/prendus-course-homepage/prendus-course-homepage.html', 'course-homepage');
+        this.importElement('components/prendus-course-homepage/prendus-course-homepage.html', 'course-homepage');
         break;
       }
 
       case 'view': {
-        this.importHref('components/prendus-course-view/prendus-course-view.html', 'course-view');
+        this.importElement('components/prendus-course-view/prendus-course-view.html', 'course-view');
         break;
       }
 
       case 'edit-quiz': {
-        this.importHref('components/prendus-edit-quiz-router/prendus-edit-quiz-router.html', 'edit-quiz');
+        this.importElement('components/prendus-edit-quiz-router/prendus-edit-quiz-router.html', 'edit-quiz');
         break;
       }
 
       case 'view-quiz': {
-        this.importHref('components/prendus-view-quiz-router/prendus-view-quiz-router.html', 'view-quiz');
+        this.importElement('components/prendus-view-quiz-router/prendus-view-quiz-router.html', 'view-quiz');
         break;
       }
 
       case 'view-video': {
-        this.importHref('components/prendus-view-video-router/prendus-view-video-router.html', 'view-video');
+        this.importElement('components/prendus-view-video-router/prendus-view-video-router.html', 'view-video');
         break;
       }
 
@@ -51,7 +50,7 @@ class PrendusCourseRouter {
     }
   }
 
-  importHref(path: string, id: string): void {
+  importElement(path: string, id: string): void {
     if(!Polymer.isInstance(this.querySelector(`#${id}`))) {
       Polymer.Base.importHref(
         path,
