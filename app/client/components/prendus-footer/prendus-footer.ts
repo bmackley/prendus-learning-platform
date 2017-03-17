18091a9703461d536c5130b57e9191ae173ba120
Brandon Blatter <intcreator@users.noreply.github.com>
2017-03-17 11:35:14 -0600

    Better fix for home page iron-list

--- app/client/components/prendus-footer/prendus-footer.ts
+++ app/client/components/prendus-footer/prendus-footer.ts
@@ -3,7 +3,6 @@ import {Actions} from '../../redux/actions';
 class PrendusFooter {
   public is: string;
   public fire: any;
-	public route: any;
 
   beforeRegister(): void {
     this.is = 'prendus-footer';
@@ -13,13 +12,6 @@ class PrendusFooter {
     const currentDate: Date = new Date();
     return currentDate.getFullYear();
   }
-
-	/**
-	 * Don't show the footer on the homepage; it's a pain with the iron-list
-	 */
-	showFooter(page: string): boolean {
-		return page !== '';
-	}
 
 }
 
