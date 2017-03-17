b4b06d333467ae070717fd1f576484b46a61b11a
Brandon Blatter <intcreator@users.noreply.github.com>
2017-03-16 22:14:01 -0600

    Fix double scrolling on home page

--- app/client/components/prendus-footer/prendus-footer.ts
+++ app/client/components/prendus-footer/prendus-footer.ts
@@ -3,6 +3,7 @@ import {Actions} from '../../redux/actions';
 class PrendusFooter {
   public is: string;
   public fire: any;
+	public route: any;
 
   beforeRegister(): void {
     this.is = 'prendus-footer';
@@ -12,6 +13,13 @@ class PrendusFooter {
     const currentDate: Date = new Date();
     return currentDate.getFullYear();
   }
+
+	/**
+	 * Don't show the footer on the homepage; it's a pain with the iron-list
+	 */
+	showFooter(page: string): boolean {
+		return page !== '';
+	}
 
 }
 
