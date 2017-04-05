731fde76d19aa7a29e7556cfd8fc6d218df8b52f
Brandon Blatter <intcreator@users.noreply.github.com>
2017-04-05 13:02:12 -0600

    Update navbar

--- app/client/components/prendus-navbar/prendus-navbar.ts
+++ app/client/components/prendus-navbar/prendus-navbar.ts
@@ -22,19 +22,6 @@ export class PrendusNavbar {
     this.username = state.currentUser.metaData.email;
   }
 
-	toggleMenuWithKeyboard(e: any): void {
-		if(e.keyCode === 13 || e.keyCode === 32) {
-			e.preventDefault();
-			this.toggleMenu(e);
-		}
-	}
-
-  toggleMenu(e: any): void {
-		this.querySelector('#menu-name-button').blur();
-		this.querySelector('#menu-icon-button').blur();
-		this.querySelector('#menu-items').toggle();
-  }
-
   logOutUser(e: any): void {
     Actions.logOutUser(this);
   }
