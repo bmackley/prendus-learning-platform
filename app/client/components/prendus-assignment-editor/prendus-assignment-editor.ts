3a03422ab7b96c4f9b6db28c9e3a1dd25358771d
Jordan Last <jordan.michael.last@gmail.com>
2017-05-19 18:35:26 -0600

    progress on adding concepts to assignment

--- app/client/components/prendus-assignment-editor/prendus-assignment-editor.ts
+++ app/client/components/prendus-assignment-editor/prendus-assignment-editor.ts
@@ -1,4 +1,8 @@
 import {Assignment} from '../../node_modules/prendus-services/typings/assignment';
+import {Taxonomy} from '../../node_modules/prendus-services/typings/taxonomy';
+import {Concept} from '../../node_modules/prendus-services/typings/concept';
+import {FirebaseService} from '../../node_modules/prendus-services/services/firebase-service';
+import {UtilitiesService} from '../../node_modules/prendus-services/services/utilities-service';
 
 class PrendusAssignmentEditor {
     public is: string;
@@ -7,6 +11,12 @@ class PrendusAssignmentEditor {
     public dispatchEvent: any;
     public lessonId: string;
     public assignment: Assignment;
+    public conceptTitles: string[];
+    public taxonomies: {
+        [uuid: string]: Taxonomy;
+    };
+    public showLearningStructureComponent: boolean;
+    public selectConceptButtonText: string;
 
     beforeRegister() {
         this.is = 'prendus-assignment-editor';
@@ -15,9 +25,16 @@ class PrendusAssignmentEditor {
                 type: String
             },
             assignment: {
-                type: Object
+                type: Object,
+                observer: 'assignmentSet'
             }
         };
+    }
+
+    ready() {
+        this.stateChange({
+            selectConceptButtonText: 'Select Concept'
+        });
     }
 
     open() {
@@ -30,7 +47,7 @@ class PrendusAssignmentEditor {
             lessonId: this.assignment ? this.assignment.lessonId : this.lessonId,
             uid: this.assignment ? this.assignment.uid : 'NOT_IMPLEMENTED',
             title: this.querySelector('#titleInput').value,
-            taxonomies: {}
+            taxonomies: this.taxonomies || {}
         };
 
         this.dispatchEvent(new CustomEvent('saveclick', {
@@ -38,6 +55,61 @@ class PrendusAssignmentEditor {
                 assignment
             }
         }));
+    }
+
+    async assignmentSet(newValue: Assignment, oldValue: Assignment) {
+        const conceptIds = Object.values(newValue ? newValue.taxonomies : {}).map((taxonomy: Taxonomy) => taxonomy.concept);
+        const conceptTitles = await UtilitiesService.asyncMap(conceptIds, async (conceptId: string) => {
+            const concept: Concept = await FirebaseService.get(`concepts/${conceptId}`);
+            return concept.title;
+        });
+
+        this.stateChange({
+            taxonomies: newValue ? newValue.taxonomies : null,
+            conceptTitles
+        });
+    }
+
+    selectConceptTap() {
+        this.stateChange({
+            showLearningStructureComponent: !this.showLearningStructureComponent,
+            selectConceptButtonText: 'Close'
+        });
+    }
+
+    async addTap() {
+        const learningStructureComponent = this.querySelector(`#learningStructureComponent`);
+        const uuid: string = UtilitiesService.createUUID();
+        const newTaxonomy: Taxonomy = {
+            discipline: learningStructureComponent.chosenDiscipline.id,
+            subject: learningStructureComponent.chosenSubject.id,
+            concept: learningStructureComponent.chosenConcept.id
+        };
+        const newTaxonomies = this.taxonomies ? {
+            ...this.taxonomies,
+            [uuid]: newTaxonomy
+        } : {
+            [uuid]: newTaxonomy
+        };
+        const newConceptIds = Object.values(newTaxonomies).map((taxonomy: Taxonomy) => taxonomy.concept);
+        const newConceptTitles = await UtilitiesService.asyncMap(newConceptIds, async (conceptId: string) => {
+            const concept: Concept = await FirebaseService.get(`concepts/${conceptId}`);
+            return concept.title;
+        });
+
+        this.stateChange({
+            taxonomies: newTaxonomies,
+            conceptTitles: newConceptTitles,
+            showLearningStructureComponent: false
+        });
+    }
+
+    //TODO this is a foreshadowing of how we will handle local state changes in the future
+    stateChange(stateChangeAction: StateChangeAction) {
+        this.taxonomies = stateChangeAction.taxonomies || this.taxonomies;
+        this.conceptTitles = stateChangeAction.conceptTitles || this.conceptTitles;
+        this.showLearningStructureComponent = stateChangeAction.showLearningStructureComponent;
+        this.selectConceptButtonText = stateChangeAction.selectConceptButtonText || this.selectConceptButtonText;
     }
 }
 
